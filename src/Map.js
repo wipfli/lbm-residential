a47a993156624512e2098278b0c07ecfa61227a3
wipfli <mail@example.com>
2021-03-12 19:58:22 +0100

    Add togglee checkbox to show residential layer

--- src/Map.js
+++ src/Map.js
@@ -12,6 +12,7 @@ const Map = ({
 }) => {
     const mapContainerRef = useRef(null)
     const [map, setMap] = useState(null)
+    const [showResidential, setShowResidential] = useState(false)
 
     useEffect(() => {
         const map = new maplibregl.Map({
@@ -29,17 +30,46 @@ const Map = ({
         setMap(map)
     }, [])
 
+    useEffect(() => {
+        if (map) {
+            map.setLayoutProperty(
+                'landuse-residential',
+                'visibility',
+                showResidential ? 'visible' : 'none'
+            )
+        }
+    }, [showResidential])
+
     return (
-        <div
-            ref={mapContainerRef}
-            style={{
-                position: 'absolute',
-                left: 0,
-                right: 0,
-                top: 0, 
-                bottom: 0,
-            }}
-        />
+        <div>
+            <div
+                ref={mapContainerRef}
+                style={{
+                    position: 'absolute',
+                    left: 0,
+                    right: 0,
+                    top: 0,
+                    bottom: 0,
+                }}
+            />
+            <div
+                style={{
+                    position: 'absolute',
+                    left: 0,
+                    top: 0,
+                    backgroundColor: 'white',
+                    margin: 10,
+                    padding: 10,
+                }}
+            >
+                <input
+                    type="checkbox"
+                    checked={showResidential}
+                    onChange={() => setShowResidential(value => !value)}
+                />
+                show landuse=residential
+            </div>
+        </div>
     )
 }
 
